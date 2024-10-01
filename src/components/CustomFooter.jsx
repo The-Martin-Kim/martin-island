c5f3711836c4cc03d33e48760155b45d65b06b6f
Martin Kim <iu2407@naver.com>
2024-10-01 22:10:52 +0900

    [fix] footer 수정

--- src/components/CustomFooter.jsx
+++ src/components/CustomFooter.jsx
@@ -5,7 +5,7 @@ function CustomFooter() {
     return (
         <footer style={{ backgroundColor: '#f8f9fa', padding: '10px 0', textAlign: 'center' }}>
             <Container>
-                <p className="mb-0">Copyright 2024. INE.TODAY. All Rights Reserved.</p>
+                <p className="mb-0">Copyright 2024. 개발하는 꼬망이. All Rights Reserved.</p>
             </Container>
         </footer>
     );
