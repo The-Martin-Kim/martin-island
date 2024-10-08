a6b910e453710316abe83bc4823794d83df3fd22
Martin Kim <iu2407@naver.com>
2024-10-08 16:08:07 +0900

    [lecture] 문제 티어 정보 추가

--- src/Lectures/ProblemData.jsx
+++ src/Lectures/ProblemData.jsx
@@ -5,40 +5,40 @@ export const problemData = [
             {
                 title: "1. 배열과 리스트 그리고 벡터",
                 problems: [
-                    {num: 11720, title: "숫자의 합"},
-                    {num: 1546, title: "평균"},
+                    {num: 11720, title: "숫자의 합 (브론즈 4)"},
+                    {num: 1546, title: "평균 (브론즈 1)"},
                 ],
             },
             {
                 title: "2. 구간 합",
                 problems: [
-                    {num: 11659, title: "구간 합 구하기 4"},
-                    {num: 11660, title: "구간 합 구하기 5"},
-                    {num: 10986, title: "나머지 합"},
+                    {num: 11659, title: "구간 합 구하기 4 (실버 3)"},
+                    {num: 11660, title: "구간 합 구하기 5 (실버 1)"},
+                    {num: 10986, title: "나머지 합 (골드 3)"},
                 ],
             },
             {
                 title: "3. 투 포인터",
                 problems: [
-                    {num: 2018, title: "수들의 합 5"},
-                    {num: 1940, title: "주몽"},
-                    {num: 1253, title: "좋다"},
+                    {num: 2018, title: "수들의 합 5 (실버 5)"},
+                    {num: 1940, title: "주몽 (실버 4)"},
+                    {num: 1253, title: "좋다 (골드 4)"},
                 ],
             },
             {
                 title: "4. 슬라이딩 윈도우",
                 problems: [
-                    {num: 12891, title: "DNA 비밀번호"},
-                    {num: 11003, title: "최솟값 찾기"},
+                    {num: 12891, title: "DNA 비밀번호 (실버 2)"},
+                    {num: 11003, title: "최솟값 찾기 (플래티넘 5)"},
                 ],
             },
             {
                 title: "5. 스택과 큐",
                 problems: [
-                    {num: 1874, title: "스택 수열"},
-                    {num: 17298, title: "오큰수"},
-                    {num: 2164, title: "카드2"},
-                    {num: 11286, title: "절댓값 힙"},
+                    {num: 1874, title: "스택 수열 (실버 2)"},
+                    {num: 17298, title: "오큰수 (골드 4)"},
+                    {num: 2164, title: "카드2 (실버 4)"},
+                    {num: 11286, title: "절댓값 힙 (실버 1)"},
                 ],
             },
         ],
@@ -49,39 +49,39 @@ export const problemData = [
             {
                 title: "1. 버블 정렬",
                 problems: [
-                    {num: 2750, title: "수 정렬하기"},
-                    {num: 1377, title: "버블 소트"},
+                    {num: 2750, title: "수 정렬하기 (브론즈 2)"},
+                    {num: 1377, title: "버블 소트 (골드 2)"},
                 ],
             },
             {
                 title: "2. 선택 정렬",
                 problems: [
-                    {num: 1427, title: "소트인사이드"},
+                    {num: 1427, title: "소트인사이드 (실버 5)"},
                 ],
             },
             {
                 title: "3. 삽입 정렬",
                 problems: [
-                    {num: 11399, title: "ATM"},
+                    {num: 11399, title: "ATM (실버 4)"},
                 ],
             },
             {
                 title: "4. 퀵 정렬",
                 problems: [
-                    {num: 11004, title: "k번째 수 구하기"},
+                    {num: 11004, title: "k번째 수 구하기 (실버 5)"},
                 ],
             },
             {
                 title: "5. 병합 정렬",
                 problems: [
-                    {num: 2751, title: "수 정렬하기 2"},
-                    {num: 1517, title: "버블 소트"},
+                    {num: 2751, title: "수 정렬하기 2 (실버 5)"},
+                    {num: 1517, title: "버블 소트 (플래티넘 5)"},
                 ],
             },
             {
                 title: "6. 기수 정렬",
                 problems: [
-                    {num: 10989, title: "수 정렬하기 3"},
+                    {num: 10989, title: "수 정렬하기 3 (브론즈 1)"},
                 ],
             },
         ],
@@ -92,25 +92,25 @@ export const problemData = [
             {
                 title: "1. 깊이 우선 탐색",
                 problems: [
-                    {num: 11724, title: "연결 요소의 개수"},
-                    {num: 2023, title: "신기한 소수"},
-                    {num: 13023, title: "ABCDE"},
+                    {num: 11724, title: "연결 요소의 개수 (실버 2)"},
+                    {num: 2023, title: "신기한 소수 (골드 5)"},
+                    {num: 13023, title: "ABCDE (골드 5)"},
                 ],
             },
             {
                 title: "2. 너비 우선 탐색",
                 problems: [
-                    {num: 1260, title: "DFS와 BFS 프로그램"},
-                    {num: 2178, title: "미로 탐색"},
-                    {num: 1167, title: "트리의 지름"},
+                    {num: 1260, title: "DFS와 BFS 프로그램 (실버 2)"},
+                    {num: 2178, title: "미로 탐색 (실버 1)"},
+                    {num: 1167, title: "트리의 지름 (골드 2)"},
                 ],
             },
             {
                 title: "3. 이진 탐색",
                 problems: [
-                    {num: 1920, title: "수 찾기"},
-                    {num: 2343, title: "기타 레슨"},
-                    {num: 1300, title: "k번째 수"},
+                    {num: 1920, title: "수 찾기 (실버 4)"},
+                    {num: 2343, title: "기타 레슨 (실버 1)"},
+                    {num: 1300, title: "k번째 수 (골드 1)"},
                 ],
             },
         ],
@@ -121,11 +121,11 @@ export const problemData = [
             {
                 title: "그리디 알고리즘",
                 problems: [
-                    {num: 11047, title: "동전 0"},
-                    {num: 1715, title: "카드 정렬하기"},
-                    {num: 1744, title: "수 묶기"},
-                    {num: 1931, title: "회의실 배정하기"},
-                    {num: 1541, title: "잃어버린 괄호"},
+                    {num: 11047, title: "동전 0 (실버 4)"},
+                    {num: 1715, title: "카드 정렬하기 (골드 4)"},
+                    {num: 1744, title: "수 묶기 (골드 4)"},
+                    {num: 1931, title: "회의실 배정하기 (실버 1)"},
+                    {num: 1541, title: "잃어버린 괄호 (실버 2)"},
                 ],
             },
         ],
@@ -136,24 +136,24 @@ export const problemData = [
             {
                 title: "1. 소수 구하기",
                 problems: [
-                    {num: 1929, title: "소수 구하기"},
-                    {num: 1456, title: "거의 소수"},
-                    {num: 1747, title: "소수 & 팰린드롬"},
-                    {num: 1016, title: "제곱 ㄴㄴ 수"},
+                    {num: 1929, title: "소수 구하기 (실버 3)"},
+                    {num: 1456, title: "거의 소수 (골드 5)"},
+                    {num: 1747, title: "소수 & 팰린드롬 (실버 1)"},
+                    {num: 1016, title: "제곱 ㄴㄴ 수 (골드 1)"},
                 ],
             },
             {
                 title: "2. 오일러 피",
                 problems: [
-                    {num: 11689, title: "GCD(N, K) = 1"},
+                    {num: 11689, title: "GCD(N, K) = 1 (골드 1)"},
                 ],
             },
             {
                 title: "3. 유클리드 호제법",
                 problems: [
-                    {num: 1934, title: "최소공배수"},
-                    {num: 1850, title: "최대공약수"},
-                    {num: 1033, title: "칵테일"},
+                    {num: 1934, title: "최소공배수 (브론즈 1)"},
+                    {num: 1850, title: "최대공약수 (실버 1)"},
+                    {num: 1033, title: "칵테일 (골드 2)"},
                 ],
             },
             {
