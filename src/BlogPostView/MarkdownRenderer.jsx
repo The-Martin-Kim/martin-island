dc989474dc04d8ad398c349408b97d5f78f218e5
Martin Kim <iu2407@naver.com>
2024-10-09 14:05:18 +0900

    [feat] 코드 하이라이터 (prism) 추가

--- src/BlogPostView/MarkdownRenderer.jsx
+++ src/BlogPostView/MarkdownRenderer.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable jsx-a11y/heading-has-content */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import gfm from 'remark-gfm';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-cpp';  // C++ 하이라이팅 모듈
+
 
 const commonStyle = { fontFamily: 'GowunDodum-Regular, sans-serif' };
 
@@ -20,20 +25,40 @@ const markdownComponents = {
     ol: (props) => <ol style={commonStyle} {...props} />,
     li: (props) => <li style={commonStyle} {...props} />,
     blockquote: (props) => <blockquote style={commonStyle} {...props} />,
-    code: (props) => <code style={{ fontFamily: 'jetbrains-mono-regular, sans-serif', fontSize: '1.1em' }} {...props} />,
+    //code: (props) => <code style={{ fontFamily: 'jetbrains-mono-regular, sans-serif', fontSize: '1.1em' }} {...props} />,
+    code: ({ node, inline, className, children, ...props }) => {
+        const match = /language-(\w+)/.exec(className || '');
+        return !inline && match ? (
+            <pre className={`language-${match[1]}`} style={{ ...commonStyle }}>
+                <code className={`language-${match[1]}`} {...props}>
+                    {children}
+                </code>
+            </pre>
+        ) : (
+            <code style={{ fontFamily: 'jetbrains-mono-regular, sans-serif', fontSize: '1.1em' }} {...props}>
+                {children}
+            </code>
+        );
+    },
     pre: (props) => <pre style={commonStyle} {...props} />,
     strong: (props) => <strong style={commonStyle} {...props} />,
     em: (props) => <em style={commonStyle} {...props} />,
 };
 
-const MarkdownRenderer = ({ content }) => (
-    <ReactMarkdown
-        remarkPlugins={[gfm]}
-        rehypePlugins={[rehypeRaw]}
-        components={markdownComponents}
-    >
-        {content}
-    </ReactMarkdown>
-);
+const MarkdownRenderer = ({ content }) => {
+    useEffect(() => {
+        Prism.highlightAll();
+    }, [content]);
+
+    return (
+        <ReactMarkdown
+            remarkPlugins={[gfm]}
+            rehypePlugins={[rehypeRaw]}
+            components={markdownComponents}
+        >
+            {content}
+        </ReactMarkdown>
+    );
+};
 
 export default MarkdownRenderer;
