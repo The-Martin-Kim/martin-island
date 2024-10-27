3616a7e3b5e91c70257da54c4f1c95ee40f46980
Martin Kim <iu2407@naver.com>
2024-10-27 10:24:28 +0900

    [feat] Main Page에 시계 추가

--- src/BlogListView/ProfileSection.jsx
+++ src/BlogListView/ProfileSection.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Grid } from '@mui/material';
 import ProfileCard from './ProfileCard';
 import CategoryFilter from './CategoryFilter';
+import Clock from "react-live-clock";
 
 function ProfileSection({ categories, selectedCategory, handleCategoryClick }) {
     return (
         <Grid item xs={12} sm={3}>
-            <ProfileCard />
+            <ProfileCard/>
+            <h1 style={{textAlign: "center", paddingBottom: "10px"}}>
+                <Clock format="h:mm:ss a" interval={1000} ticking={true}/>
+            </h1>
             <CategoryFilter
                 categories={categories}
                 selectedCategory={selectedCategory}
