d26d4d78bde5b5e765bed2f052fcaaa891868b4e
Martin Kim <iu2407@naver.com>
2024-10-27 10:27:54 +0900

    [delete] martin-island 최적화 작업

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -6,22 +6,7 @@ function CustomNavbar() {
     return (
         <Navbar bg="light" fixed="top">
             <Container>
-                <Navbar.Brand href="/"
-                              style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                    <>
-                        <img
-                            alt=""
-                            src="/logo.png"
-                            width={"30"}
-                            height={"30"}
-                            className="d-inline-block align-top"
-                        />
-                        <div className="GowunDodum-Regular" style={{textAlign: 'center', marginLeft: "10px"}}>
-                            <strong>개발하는 꼬망이</strong>
-                        </div>
-                    </>
-                </Navbar.Brand>
-                <Nav className="justify-content-end" activeKey="/home">
+                <Nav className="me-auto" activeKey="/home">
                     <Nav.Item>
                         <Nav.Link href="/" className="GowunDodum-Regular">Home</Nav.Link>
                     </Nav.Item>
@@ -29,9 +14,18 @@ function CustomNavbar() {
                         <Nav.Link href="/about" className="GowunDodum-Regular">Profile</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="/lecture" className="GowunDodum-Regular">Lecture</Nav.Link>
+                        <Nav.Link href="/lecture" className="GowunDodum-Regular">Q-bank</Nav.Link>
                     </Nav.Item>
                 </Nav>
+                <Navbar.Brand href="/"
+                              className="ms-auto">
+                    <div style={{display: 'flex', alignItems: 'center'}}>
+                        <div className="GowunDodum-Regular" style={{textAlign: 'center', marginLeft: "10px"}}>
+                            <strong>Martin-Island.com</strong>
+                        </div>
+                    </div>
+                </Navbar.Brand>
+
             </Container>
         </Navbar>
     );
